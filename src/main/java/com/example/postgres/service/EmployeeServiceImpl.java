556eca09e9db3a14ed6379247d9b9cc74c4bbe25
kimD0ngjun <chickentasty0112@gmail.com>
2024-05-20 15:36:26 +0900

    feat: EmployeeService 신규 생성 로직 작성

--- src/main/java/com/example/postgres/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/postgres/service/EmployeeServiceImpl.java
@@ -1,12 +1,24 @@
 package com.example.postgres.service;
 
 import com.example.postgres.dto.EmployeeDto;
+import com.example.postgres.entity.Employee;
+import com.example.postgres.mapper.EmployeeMapper;
+import com.example.postgres.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+
+        Employee employee = EmployeeMapper.mapTOEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 }

d6a358f8abb1921b0f3ec2651208844f809614a2
kimD0ngjun <chickentasty0112@gmail.com>
2024-05-20 17:27:57 +0900

    feat: EmployeeService 전체 조회 추상 및 구현 메소드 작성

--- src/main/java/com/example/postgres/service/EmployeeService.java
+++ src/main/java/com/example/postgres/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.example.postgres.service;
 
 import com.example.postgres.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/com/example/postgres/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/postgres/service/EmployeeServiceImpl.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Service
 @AllArgsConstructor
@@ -34,5 +36,12 @@ public class EmployeeServiceImpl implements EmployeeService {
                 );
 
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::mapToEmployeeDto).toList();
     }
 }

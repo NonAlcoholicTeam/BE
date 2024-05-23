9523f0ad425226c9d9923a3bdf0eae560d3c7f46
kimD0ngjun <chickentasty0112@gmail.com>
2024-05-23 17:31:19 +0900

    feat: 스프링 시큐리티 설정 클래스 프레임 생성

---
+++ src/main/java/com/example/mini_project/auth/configuration/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.mini_project.auth.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity // Spring Security 지원을 가능하게 함
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+}

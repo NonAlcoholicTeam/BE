9b2abb803e2822fa5d75c672becb4c5ea9788094
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-02 16:21:17 +0900

    fix: 데이터베이스 조회 검증 작업 추가

--- src/main/java/com/example/mini_project/global/auth/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/example/mini_project/global/auth/jwt/JwtAuthorizationFilter.java
@@ -3,6 +3,7 @@ package com.example.mini_project.global.auth.jwt;
 import com.example.mini_project.domain.entity.User;
 import com.example.mini_project.domain.entity.UserDetailsServiceImpl;
 import com.example.mini_project.domain.repository.UserRepository;
+import com.example.mini_project.global.auth.entity.Token;
 import com.example.mini_project.global.auth.entity.TokenType;
 import com.example.mini_project.global.auth.repository.TokenRepository;
 import com.example.mini_project.global.exception.ResourceNotFoundException;
@@ -22,6 +23,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j(topic = "JWT 검증 및 인가")
 @RequiredArgsConstructor
@@ -65,15 +67,32 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
             String email = info.getSubject();
 
-            User user = userRepository.findByEmail(email).get();
+            Optional<User> userOptional = userRepository.findByEmail(email);
+
+            if (userOptional.isEmpty()) {
+                throw new ResourceNotFoundException("비정상적인 이메일 정보. 재확인 바람.");
+            }
+
+            User user = userOptional.get();
+            Optional<Token> tokenOptional = tokenRepository.findByUser(user);
 
             // 데이터베이스에 저장되어있는지 확인하기
-            if (tokenRepository.findByUser(user).isEmpty()) {
-                throw new ResourceNotFoundException("비정상적인 이메일 정보. 재확인 바람.");
+            if (tokenOptional.isEmpty()) {
+                throw new ResourceNotFoundException("비정상적인 토큰 정보. 재확인 바람.");
+            }
+
+            Token tokenObj = tokenOptional.get();
+
+            // 데이터베이스의 내용과 일치하는지 확인하기
+            if (!tokenObj.getRefreshToken().equals(refreshToken)) {
+                throw new ResourceNotFoundException("일치하는 토큰 정보 찾을 수 없음. 재확인 바람.");
             }
 
             String newAccessToken = jwtUtil.createAccessToken(
                     jwtUtil.createTokenPayload(user.getEmail(), user.getRole(), TokenType.ACCESS));
+            log.info("새로운 엑세스토큰: " + newAccessToken);
+            // 새로운 엑세스토큰 업데이트
+            tokenObj.update(newAccessToken);
 
             try {
                 // username 담아주기
@@ -98,6 +117,28 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             }
 
             Claims info = jwtUtil.getUserInfoFromToken(accessToken);
+            String email = info.getSubject();
+
+            Optional<User> userOptional = userRepository.findByEmail(email);
+
+            if (userOptional.isEmpty()) {
+                throw new ResourceNotFoundException("비정상적인 이메일 정보. 재확인 바람.");
+            }
+
+            User user = userOptional.get();
+            Optional<Token> tokenOptional = tokenRepository.findByUser(user);
+
+            // 데이터베이스에 저장되어있는지 확인하기
+            if (tokenOptional.isEmpty()) {
+                throw new ResourceNotFoundException("비정상적인 토큰 정보. 재확인 바람.");
+            }
+
+            Token tokenObj = tokenOptional.get();
+
+            // 데이터베이스의 내용과 일치하는지 확인하기
+            if (!tokenObj.getAccessToken().equals(accessToken)) {
+                throw new ResourceNotFoundException("일치하는 토큰 정보 찾을 수 없음. 재확인 바람.");
+            }
 
             try {
                 // username 담아주기
